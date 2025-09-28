924d8a1b3edfa05f9063c8126531624b8387e87f
hrafique4 <chamzah16@gmail.com>
2025-09-27 22:53:25 -0400

    Working ESLint v9 + Prettier + Jest config

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: globals.node
+    },
+    ignores: ["node_modules/**", "coverage/**", "dist/**", "build/**"]
+  }
+];
